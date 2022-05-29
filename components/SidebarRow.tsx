272069a36e50e42663e16dfc49665a49fb72e7bf
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-05-28 23:21:05 -0300

    Improve Sidebar

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,16 +1,23 @@
 import { SVGProps } from "react"
+import { useRouter } from "next/router";
 
 interface Props{
     Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element,
     title: string,
+    active?: Boolean
 }
 
 
-function SidebarRow({Icon , title}: Props) {
+function SidebarRow({Icon , title , active}: Props) {
+  const router = useRouter();
   return (
-    <div className="flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200 group">
+    <div className={`flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200 group ${
+      active && "font-bold"
+    }`}
+    onClick={() => active && router.push("/")}
+    >
         <Icon className="h-6 w-6"/>    
-        <p className="group-hover:text-twitter">{title}</p>
+        <p className="hidden xl:inline group-hover:text-twitter">{title}</p>
     </div>
   )
 }
