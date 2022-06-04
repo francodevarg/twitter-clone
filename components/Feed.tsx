1692ac82bb6dbb86e3e855017c15490a2a59548f
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-06-04 19:08:43 -0300

    Component Input has been styled

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -36,7 +36,7 @@ function Feed() {
 
   return (
     <div className="flex-grow border-l border-r border-gray-200 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 border-b border-gray-200">
+      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 border-b border-gray-200 bg-white">
         <h2 className="text-lg sm:text-xl font-bold text-gray-700">Home</h2>
         <div className="hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto">
           <SparklesIcon className="h-5 text-gray-700" />
