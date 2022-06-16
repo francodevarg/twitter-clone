b72f203d8de4a4ec7700baf22072967c5bb2e79e
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-06-16 20:21:51 -0300

    Upload Post Functionality Completed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,9 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from "react";
 import Input from "./Input";
-// import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import {db} from '../firebase'
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import Post from "./Post";
 //import { useSession } from "next-auth/react";
 
 function Feed() {
@@ -22,17 +24,17 @@ function Feed() {
   //   };
   // }, [db]);
 
-  // CLEAN
-  // useEffect(
-  //   () =>
-  //     onSnapshot(
-  //       query(collection(db, "posts"), orderBy("timestamp", "desc")),
-  //       (snapshot) => {
-  //         setPosts(snapshot.docs);
-  //       }
-  //     ),
-  //   [db]
-  // );
+  //CLEAN
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
 
   return (
     <div className="flex-grow border-l border-r border-gray-200 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
@@ -45,9 +47,9 @@ function Feed() {
 
       <Input />
       <div className="pb-[1300px]">
-        {/* {posts.map((post) => (
-          // <Post key={post.id} id={post.id} post={post.data()} />
-        ))} */}
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
       </div>
     </div>
   );
--- components/Input.tsx
+++ components/Input.tsx
@@ -74,90 +74,93 @@ function Input() {
 
   return (
     <div
-      className={`flex space-x-3 overflow-hidden border-b border-gray-200 p-3`}
+      className={`border-b border-gray-200 p-3 flex space-x-3 ${
+        loading && "opacity-60"
+      }`}
     >
       <img
-        src={
-          'https://yt3.ggpht.com/yti/APfAmoFEvwA525FagnnvCJ7dcCmwp2P1AOLWo9yBCzgVzO8=s88-c-k-c0x00ffffff-no-rj-mo'
-        }
-        className="h-11 w-11 cursor-pointer rounded-full"
+        src={'https://yt3.ggpht.com/yti/APfAmoFEvwA525FagnnvCJ7dcCmwp2P1AOLWo9yBCzgVzO8=s88-c-k-c0x00ffffff-no-rj-mo'}
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+        // onClick={signOut}
       />
-
-      <div className="w-full divide-y divide-gray-200">
-        <div className={``}>
+      <div className="divide-y divide-gray-200 w-full">
+        <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
           <textarea
-            className="min-h-[50px] w-full bg-transparent text-lg tracking-wide text-gray-700 placeholder-gray-500 outline-none"
-            placeholder="What's happening?"
-            onChange={(e) => setInput(e.target.value)}
-            name=""
-            id=""
-            rows={2}
             value={input}
-          ></textarea>
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="What's happening?"
+            rows="2"
+            className="bg-transparent outline-none text-[#000] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
+          />
+
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
         </div>
+        {!loading && (
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center">
+              <div
+                className="icon"
+                onClick={() => filePickerRef.current.click()}
+              >
+                <PhotographIcon className="text-[#1d9bf0] h-[22px]" />
+                <input
+                  type="file"
+                  ref={filePickerRef}
+                  hidden
+                  onChange={addImageToPost}
+                />
+              </div>
 
-        {/** Selected File  */}
-        <div className="relative">
-          {/* <div className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer">
+              <div className="icon rotate-90">
+                <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
 
-                    </div> */}
-        </div>
+              <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                <EmojiHappyIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
 
-        <div className="flex items-center justify-between pt-2.5">
-          <div className="flex items-center">
-            <div
-              className="icon"
-              // onClick={() => filePickerRef.current.click()
-              //}
+              <div className="icon">
+                <CalendarIcon className="text-[#1d9bf0] h-[22px]" />
+              </div>
+
+              {showEmojis && (
+                <Picker
+                  onSelect={addEmoji}
+                  style={{
+                    position: "absolute",
+                    marginTop: "465px",
+                    marginLeft: -40,
+                    maxWidth: "320px",
+                    borderRadius: "20px",
+                  }}
+                  theme="dark"
+                />
+              )}
+            </div>
+            <button
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              disabled={!input && !selectedFile}
+              onClick={sendPost}
             >
-              <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
-              <input
-                type="file"
-                ref={filePickerRef}
-                hidden
-                onChange={addImageToPost}
-              />
-            </div>
-
-            <div className="icon rotate-90">
-              <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-
-            <div
-              className="icon"
-              // onClick={() => setShowEmojis(!showEmojis)}
-            >
-              <EmojiHappyIcon
-                className="h-[22px] text-[#1d9bf0]"
-                onClick={() => setShowEmojis(!showEmojis)}
-              />
-            </div>
-
-            <div className="icon">
-              <CalendarIcon className="h-[22px] text-[#1d9bf0]" />
-            </div>
-
-            {showEmojis && (
-              <Picker
-                onSelect={addEmoji}
-                style={{
-                  position: 'absolute',
-                  marginTop: '465px',
-                  marginLeft: -40,
-                  maxWidth: '320px',
-                  borderRadius: '20px',
-                }}
-              />
-            )}
+              Tweet
+            </button>
           </div>
-          <button
-            className="rounded-full bg-[#1d9bf0] px-4 py-1.5 font-bold text-white shadow-md hover:bg-[#1a8cd8] disabled:cursor-default disabled:opacity-50 disabled:hover:bg-[#1d9bf0]"
-            disabled={!input && !selectedFile}
-            // onClick={sendPost}
-          >
-            Tweet
-          </button>
-        </div>
+        )}
       </div>
     </div>
   )
